18bb0158c62999d4370dc7d2514f199b69a1471c
Kenny Tran <trankennykhang@hotmail.com>
2022-06-11 16:40:58 +0930

    init kohana

---
+++ application/classes/Controller/Welcome.php
@@ -0,0 +1,14 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Welcome extends Controller {
+
+	public function action_index()
+	{
+		$this->response->body('hello, world!');
+	}
+	public function action_index2()
+	{
+		$this->response->body('hello, world2!');
+	}
+
+} // End Welcome
